f69cd9b48e4e948b6f83a447b34b05ad7306672c
mineshmane <mineshmane94@gmail.com>
2019-11-26 19:26:46 +0530

    commit

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
 // import React, { useState } from "react";
-import { Button, FormGroup, FormControl } from "react-bootstrap";
+import { Button, FormGroup, FormControl, Form } from "react-bootstrap";
 // import Form from 'react-bootstrap/Form'
 // import Button from 'react-bootstrap/Button'
 // import Bootstrap from "react-bootstrap";
+
+
+
+
+
+
+
 export default class LoginComponent extends Component {
+
+
+
+
 
 
     constructor(props) {
@@ -30,28 +41,67 @@ export default class LoginComponent extends Component {
         return (
 
             <div className="Login">
-                <form onSubmit={this.handleSubmit}>
-                    <FormGroup controlId="email" bsSize="large">
-                        {/* <ControlLabel>Email</ControlLabel> */}
-                        <FormControl
-                            autoFocus
-                            type="email"
-                            value={this.email}
-                            onChange={this.handleSubmit}
-                        />
-                    </FormGroup>
-                    <FormGroup controlId="password" bsSize="large">
-                        {/* <ControlLabel>Password</ControlLabel> */}
-                        <FormControl
-                            value={this.password}
-                            onChange={this.handleSubmit}
-                            type="password"
-                        />
-                    </FormGroup>
-                    <Button block bsSize="large" disabled={!this.validateForm} type="submit">
-                        Login
-        </Button>
-                </form>
+                <div className="auth-box">
+
+
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="image-center" >
+                            <img id="img-circle" className=" img-thumbnail" src={require('../assets/mi-logo.jpg')}></img>
+                        </div>
+                        <div className=" heading ">
+                            <h4> login to your account</h4>
+
+                        </div>
+                        <div>
+
+                            <FormGroup controlId="email" bsSize="large">
+
+                                <FormControl className="input"
+                                placeholder="UserName"
+                                    autoFocus
+                                    type="email"
+                                    value={this.email}
+                                    onChange={this.handleSubmit}
+                                />
+                            </FormGroup>
+                        </div>
+                        <div className="margin-top">
+
+                            <FormGroup controlId="password" bsSize="large">
+
+                                <FormControl className="input"
+                                placeholder="Password"
+                                    value={this.password}
+                                    onChange={this.handleSubmit}
+                                    type="password"
+                                />
+                            </FormGroup>
+                        </div>
+                        <div className="margin-top">
+
+                            <Button className="btn btn-primary btn-lg btn-block" disabled={!this.validateForm} type="submit">
+                                Login  </Button>
+                        </div>
+                        <div className="rowImage">
+
+                            <div>
+
+                                <img id="img-circle" className="lock-thumbnail" src={require('../assets/lock.png')}></img>
+
+                            </div>
+                            <div>
+                                <span>forgetpassword</span>
+
+                            </div>
+
+
+
+                        </div>
+
+                    </form>
+
+                </div>
+
             </div>
         )
     }
